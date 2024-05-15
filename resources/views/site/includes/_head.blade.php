731d92907a54c7a043684cbf0aba135e886715b7
Hamza AIT <hamzacodedot@gmail.com>
2024-05-15 18:28:23 +0530

    add link

--- resources/views/site/includes/_head.blade.php
+++ resources/views/site/includes/_head.blade.php
@@ -3,19 +3,28 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-  <link rel="stylesheet" href="{{ asset('assets/css/style.css' ) }}">
-  <link rel="stylesheet" href="{{ asset('assets/datepicker/css/bootstrap-datepicker.min.css' ) }}">        
-  <link rel="stylesheet" href="{{ asset('assets/datepicker/css/custom.css' ) }}">  
-  <!-- slick-slider -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>  
-        <!-- Bootstrap -->
-  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css' ) }}">
   <!-- Font Awesome -->
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-
+  {{-- <link rel="stylesheet" href="{{ asset('assets/css/all.css' ) }}"> --}}
+    <!-- Lightbox -->
+    <!-- slick-slider -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"/>
+  {{-- <link href="{{ asset('assets/css/lightbox.min.css' ) }}" rel="stylesheet"> --}}
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css' ) }}">
+  
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css">
+  <!-- Custom -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css' ) }}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
+  <link rel="stylesheet" href="{{ asset('assets/datepicker/css/bootstrap-datepicker.min.css' ) }}">        
+  <link rel="stylesheet" href="{{ asset('assets/datepicker/css/custom.css' ) }}">    
+  <link rel="stylesheet" href="https://cdn3.devexpress.com/jslib/19.2.3/css/dx.common.css" />
+<link rel="stylesheet" href="https://cdn3.devexpress.com/jslib/19.2.3/css/dx.material.orange.light.compact.css" />
 
 
 
